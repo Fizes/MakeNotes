bacccbc6175aab13bf4c540628a8a10be908cb47
Slava Karpov <marcus.fizes@gmail.com>
2018-11-06 23:40:46 +0400

    Added GUI frameworks

---
+++ MakeNotes/App.ammy.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows;
+using AmmySidekick;
+
+namespace MakeNotes
+{
+    public partial class App : Application
+    {
+        [STAThread]
+        public static void Main()
+        {
+            App app = new App();
+            app.InitializeComponent();
+
+            RuntimeUpdateHandler.Register(app, $"/{Ammy.GetAssemblyName(app)};component/App.g.xaml");
+
+            app.Run();
+        }
+    }
+}
