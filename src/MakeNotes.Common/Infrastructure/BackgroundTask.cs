98052ca699c6276230ae1ee44141cdd9a2e6317f
Slava Karpov <marcus.fizes@gmail.com>
2019-02-18 00:09:27 +0400

    Made possible to run multiple concurrent background tasks, assigned close command to toolbar Exit button

--- src/MakeNotes.Common/Infrastructure/BackgroundTask.cs
+++ src/MakeNotes.Common/Infrastructure/BackgroundTask.cs
@@ -1,24 +1,53 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
 using MakeNotes.Common.Interfaces;
 
 namespace MakeNotes.Common.Infrastructure
 {
     public class BackgroundTask : IBackgroundTask
     {
-        private readonly BackgroundWorker _backgroundWorker = new BackgroundWorker();
-
-        private Action _action;
-
-        public BackgroundTask()
-        {
-            _backgroundWorker.DoWork += (s, e) => _action?.Invoke();
-        }
+        private readonly List<BackgroundWorker> _backgroundWorkers = new List<BackgroundWorker>();
 
         public void Start(Action action)
         {
-            _action = action;
-            _backgroundWorker.RunWorkerAsync();
+            var backgroundWorker = GetFreeBackgroundWorker();
+            backgroundWorker.RunWorkerAsync(action);
+        }
+
+        private BackgroundWorker GetFreeBackgroundWorker()
+        {
+            var backgroundWorker = _backgroundWorkers.FirstOrDefault(bw => !bw.IsBusy);
+            if (backgroundWorker == null)
+            {
+                backgroundWorker = new BackgroundWorker();
+                backgroundWorker.DoWork += OnWorkerRunning;
+                backgroundWorker.RunWorkerCompleted += OnRunWorkerCompleted;
+
+                _backgroundWorkers.Add(backgroundWorker);
+            }
+
+            return backgroundWorker;
+        }
+
+        private void OnWorkerRunning(object sender, DoWorkEventArgs e)
+        {
+            var action = (Action)e.Argument;
+            action?.Invoke();
+        }
+
+        private void OnRunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (_backgroundWorkers.Count < 2)
+            {
+                return;
+            }
+
+            var backgroundWorker = (BackgroundWorker)sender;
+            backgroundWorker.DoWork -= OnWorkerRunning;
+            backgroundWorker.RunWorkerCompleted -= OnRunWorkerCompleted;
+            _backgroundWorkers.Remove(backgroundWorker);
         }
     }
 }
